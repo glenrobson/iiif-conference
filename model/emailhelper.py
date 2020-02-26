e50eded4082692be85fb8d62954baf55f7b4cd34
Glen Robson <glen.robson@gmail.com>
2020-02-26 02:09:38 +0000

    Adding GMail support

--- model/emailhelper.py
+++ model/emailhelper.py
@@ -6,6 +6,14 @@ from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+import base64
+
 from bottle import template
 
 class EmailClient:
@@ -41,17 +49,57 @@ class EmailClient:
         server.quit()
         return problems
 
+class GmailClient:
 
+    def __init__(self):
+        creds = None
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file('credentials.json', ['https://www.googleapis.com/auth/gmail.send'])
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
 
-def createEmailClient():
-    if 'SMTP' not in os.environ or 'SMTP_USER' not in os.environ or 'SMTP_PASS' not in os.environ:
-        raise EnvironmentError("You need to add SMTP, SMTP_USER adn SMTP_PASS to your enviroment")
+        self.service = build('gmail', 'v1', credentials=creds)
 
-    smtpserver = os.environ['SMTP']
-    username = os.environ['SMTP_USER']
-    password = os.environ['SMTP_PASS']
+    def send(self, from_addr, to, subject, message):
+        message["From"] = from_addr
+        message["To"] = to
+        message["Subject"] = subject
+        #print (message["From"])
 
-    return EmailClient(smtpserver, username, password)
+        raw = base64.urlsafe_b64encode(message.as_bytes())
+        raw = raw.decode()
+        body = {'raw': raw}
+        response = self.service.users().messages().send(userId="me", body=body).execute()
+        #print ("Response")
+        #print (response)
+        
+        # Need to figure out how to send an error
+        return None
+
+def createEmailClient(gmail=False):
+    if gmail:
+        return GmailClient()
+    else:
+        if 'SMTP' not in os.environ or 'SMTP_USER' not in os.environ or 'SMTP_PASS' not in os.environ:
+            raise EnvironmentError("You need to add SMTP, SMTP_USER adn SMTP_PASS to your enviroment")
+
+        smtpserver = os.environ['SMTP']
+        username = os.environ['SMTP_USER']
+        password = os.environ['SMTP_PASS']
+
+        return EmailClient(smtpserver, username, password)
 
 def createMessage(text=None,html=None,attachments=[]):
     if html is None:
@@ -78,14 +126,16 @@ def createMessage(text=None,html=None,attachments=[]):
     #print (message)
     return message
 
-def createAttachment(filename, subtype):
+def createAttachment(filename, subtype, name=None):
     """
     subtype is the content after the slash in the mimetype e.g. png for image/png
     """
+    if not name:
+        name = filename
     with open(filename, 'rb') as file:
         attachment = MIMEImage(file.read(), _subtype=subtype)
         file.close()
-        attachment.add_header('Content-Disposition', 'attachment', filename=filename)
+        attachment.add_header('Content-Disposition', 'attachment', filename=name)
         return attachment
 
 if __name__ == "__main__":
